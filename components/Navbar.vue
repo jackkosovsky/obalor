894c05802f94591f9829690de467090cc102ed28
jkristo <drr.jack@gmail.com>
2020-01-28 15:56:38 +0200

    last heroku changes

---
+++ components/Navbar.vue
@@ -0,0 +1,77 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nuxt-link class="navbar-brand" to="/">Nuxt SSR</nuxt-link>
+
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+
+        <li class="nav-item">
+          <nuxt-link exact no-prefetch active-class="active" class="nav-link" to="/">
+            Home
+            <span class="sr-only">(current)</span>
+          </nuxt-link>
+        </li>
+
+        <li class="nav-item">
+          <nuxt-link active-class="active" class="nav-link" to="/about">
+            About
+          </nuxt-link>
+        </li>
+
+        <li class="nav-item">
+          <nuxt-link active-class="active" class="nav-link" to="/users">
+            Users
+          </nuxt-link>
+        </li>
+
+        <li class="nav-item" v-if="!hasToken">
+          <nuxt-link active-class="active" class="nav-link" to="/login">
+            Login
+          </nuxt-link>
+        </li>
+
+        <li class="nav-item" v-else>
+          <a @click.prevent="logout" class="nav-link" href="#">
+            Logout
+          </a>
+        </li>
+
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <button class="enter btn my-2 my-sm-0" type="submit">Вход или регистрация</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "Navbar",
+    methods: {
+      logout() {
+        this.$store.dispatch('logout')
+        this.$router.push('/login')
+      }
+    },
+    computed: {
+      hasToken() {
+        return this.$store.getters.hasToken;
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .active {
+    color: chocolate !important;
+  }
+
+  .enter {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 24px;
+    padding: 21px 34px;
+    background-color: #2F80F3;
+  }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!--<div class="podlogka"><img src="~/static/podlogka2.png" alt=""></div>-->
+    <Banner/>
+    <Navigation/>
+    <Nuxt/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+  import Banner from '~/components/Banner.vue';
+  import Navigation from '~/components/Navigation.vue';
+  import Navbar from '~/components/Navbar.vue';
+  import Footer from '~/components/Footer.vue';
+
+  export default {
+    components: {
+      Banner,
+      Navigation,
+      Navbar,
+      Footer
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .podlogka {
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    width: 100%;
+    opacity: .4;
+  }
+</style>
